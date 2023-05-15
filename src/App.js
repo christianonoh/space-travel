dc82c85da61e119b5e3ba07a2eb14e87704da900
christianonoh <onohchibyk@gmail.com>
2023-05-15 23:27:03 +0100

    Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Layout from './components/Layout';
+import Missions from './routes/Missions';
+import Profile from './routes/Profile';
+import NotMatch from './routes/NotMatch';
+import Rockets from './routes/Rockets';
 
 function App() {
   return (
-    <header>
-      <Navbar />
-    </header>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
