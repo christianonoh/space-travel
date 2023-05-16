ba0bb6fc19e682167124ab50e1d0c7612039c90a
christianonoh <onohchibyk@gmail.com>
2023-05-16 12:00:21 +0100

    Modify Missions to use redux store

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,29 +1,9 @@
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styles from '../styles/missions.module.css';
 import Button from './Button';
 
 const Missions = () => {
-  const [missionsArr] = useState([
-    {
-      mission_id: 1,
-      mission_name: 'Mission 1',
-      description: 'The young boy, who had always been fascinated by the stars, looked up at the night sky with wonder. He had been dreaming of this moment for as long as he could remember, and now that he was finally here, he couldn\'t believe his eyes. The stars were so bright and beautiful, and he felt like he could reach out and touch them. He spent the next few hours lying on his back, gazing up at the heavens, and he knew that this was a moment that he would never forget.',
-      status: false,
-    },
-    {
-      mission_id: 2,
-      mission_name: 'Mission 2',
-      description: 'The young boy, who had always been fascinated by the stars, looked up at the night sky with wonder. He had been dreaming of this moment for as long as he could remember, and now that he was finally here, he couldn\'t believe his eyes. The stars were so bright and beautiful, and he felt like he could reach out and touch them. He spent the next few hours lying on his back, gazing up at the heavens, and he knew that this was a moment that he would never forget.',
-      status: true,
-    },
-    {
-      mission_id: 3,
-      mission_name: 'Mission 3',
-      description: 'Description for Mission 3',
-      status: false,
-    },
-    // Add more objects as needed
-  ]);
+  const missionsArr = useSelector((store) => store.missions);
 
   return (
     <section>
