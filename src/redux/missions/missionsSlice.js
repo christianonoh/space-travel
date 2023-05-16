f6d1d62db15dd73108319c661fe1f49592017cb4
christianonoh <onohchibyk@gmail.com>
2023-05-16 16:39:04 +0100

    Fetch missions data from API

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchData = createAsyncThunk('missions/fetchData', async () => {
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://api.spacexdata.com/v3/missions/',
+    headers: { },
+  };
+  const response = await axios.request(config);
+  if (response.data) {
+    return response.data;
+  }
+  return [];
+});
 
 const initialState = [
   {
@@ -25,6 +40,15 @@ const missionsReducer = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.fulfilled, (state, action) => {
+      const dataWithStatus = action.payload.map((mission) => ({
+        ...mission,
+        status: false,
+      }));
+      return dataWithStatus;
+    });
+  },
 });
 
 export default missionsReducer.reducer;
