80addc9d619965cdbca55dd3ff493a7478d2d1c8
christianonoh <onohchibyk@gmail.com>
2023-05-16 17:12:38 +0100

    Remove hardcoded state

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -15,26 +15,7 @@ export const fetchData = createAsyncThunk('missions/fetchData', async () => {
   return [];
 });
 
-const initialState = [
-  {
-    mission_id: 1,
-    mission_name: 'Mission 1',
-    description: 'The young boy, who had always been fascinated by the stars, looked up at the night sky with wonder. He had been dreaming of this moment for as long as he could remember, and now that he was finally here, he couldn\'t believe his eyes. The stars were so bright and beautiful, and he felt like he could reach out and touch them. He spent the next few hours lying on his back, gazing up at the heavens, and he knew that this was a moment that he would never forget.',
-    status: false,
-  },
-  {
-    mission_id: 2,
-    mission_name: 'Mission 2',
-    description: 'The young boy, who had always been fascinated by the stars, looked up at the night sky with wonder. He had been dreaming of this moment for as long as he could remember, and now that he was finally here, he couldn\'t believe his eyes. The stars were so bright and beautiful, and he felt like he could reach out and touch them. He spent the next few hours lying on his back, gazing up at the heavens, and he knew that this was a moment that he would never forget.',
-    status: true,
-  },
-  {
-    mission_id: 3,
-    mission_name: 'Mission 3',
-    description: 'Description for Mission 3',
-    status: false,
-  },
-];
+const initialState = [];
 
 const missionsReducer = createSlice({
   name: 'missions',
