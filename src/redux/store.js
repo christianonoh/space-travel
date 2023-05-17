1c5041f1a980845fde3a3debe38ed571e7ce61d4
zamcham <cristian@zamcham.com>
2023-05-17 17:02:41 -0400

    add import statements

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import missionsReducer from './missions/missionsSlice';
+import missionsReducer from './slicers/missionsSlice';
+import rocketSlice from './slicers/rocketSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketSlice,
   },
 });
 
