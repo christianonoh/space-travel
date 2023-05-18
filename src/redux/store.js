be041e7b42571e92c4510f80eb0bfd7dfa3dfcb8
zamcham <cristian@zamcham.com>
2023-05-18 00:27:54 -0400

    import rocketSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './slicers/missionsSlice';
-import rocketSlice from './slicers/rocketSlice';
+import rocketReducer from './slicers/rocketSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
-    rockets: rocketSlice,
+    rockets: rocketReducer,
   },
 });
 
