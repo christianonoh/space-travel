d8d685953ebf0eeb452f1fff6f9cd3996c946683
zamcham <cristian@zamcham.com>
2023-05-29 11:40:35 -0400

    Install scss dependencies and imported fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Layout from './components/Layout';
 import Missions from './routes/MissionsPage';
 import Profile from './routes/Profile';
