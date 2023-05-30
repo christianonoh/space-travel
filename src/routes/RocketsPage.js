919cb48dc38763ec01a707489b1499471659b8ea
zamcham <cristian@zamcham.com>
2023-05-30 09:27:46 -0400

    add styling

--- src/routes/RocketsPage.js
+++ src/routes/RocketsPage.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRockets, toggleReserveRocket } from '../redux/rockets/rocketSlice';
 import RocketCard from '../components/RocketCard';
+import baseBg from '../assets/baseBg.svg';
+import p1 from '../assets/p1.png';
+import p2pink from '../assets/pp1.png';
 
 const RocketsPage = () => {
   const dispatch = useDispatch();
@@ -20,19 +23,40 @@ const RocketsPage = () => {
   };
 
   return (
-    <div>
-      {rockets.map((rocket) => (
-        <RocketCard
-          key={rocket.id}
-          name={rocket.name}
-          description={rocket.description}
-          imageUrl={rocket.flickr_images[0]}
-          rocketID={rocket.id}
-          reserved={reservedRockets.includes(rocket.id)}
-          onToggleReserve={handleToggleReserve}
-        />
-      ))}
-    </div>
+    <>
+      <main>
+        <img src={baseBg} alt="Background" id="baseBg" />
+        <img src={p1} alt="Background" id="p1" />
+        <img src={p2pink} alt="Background" id="p2pink" />
+        <div className="main-hero-container">
+          <div className="hero-text">
+            <h1> Space </h1>
+            <h1 className="purple-text"> Traveler </h1>
+          </div>
+          <div className="hero-text">
+            <p>
+              Looking for an unforgettable celestial journey? Take your
+              pick from our stellar selection of rockets and buckle up for
+              an epic space odyssey. Get set to launch into a galaxy of excitement!
+            </p>
+          </div>
+        </div>
+      </main>
+      <div>
+        {rockets.map((rocket) => (
+          <RocketCard
+            key={rocket.id}
+            name={rocket.name}
+            description={rocket.description}
+            imageUrl={rocket.flickr_images[0]}
+            rocketID={rocket.id}
+            reserved={reservedRockets.includes(rocket.id)}
+            onToggleReserve={handleToggleReserve}
+          />
+        ))}
+      </div>
+    </>
+
   );
 };
 
