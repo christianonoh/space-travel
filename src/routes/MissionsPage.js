8619d41ddf164dd76051d39e94688b962c7f1f5b
zamcham <cristian@zamcham.com>
2023-06-03 13:33:54 -0400

    remove unnecessary div container

--- src/routes/MissionsPage.js
+++ src/routes/MissionsPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Missions from '../components/Missions';
 
 const MissionsPage = () => (
-  <div>
+  <>
     <Missions />
-  </div>
+  </>
 );
 
 export default MissionsPage;
